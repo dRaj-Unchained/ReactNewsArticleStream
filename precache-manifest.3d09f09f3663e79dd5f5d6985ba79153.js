3c401eafdefb0abf42838ffa68afbbd9a6924a81
Raj Kumar Yadav <rajkumaryadav@spglobal.com>
2020-08-13 18:04:53 +0530

    Updates

---
+++ precache-manifest.3d09f09f3663e79dd5f5d6985ba79153.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b06c2bba7beda8c235dea3b3eca38fa",
+    "url": "/dRaj-Unchained/ReactNewsArticleStream/index.html"
+  },
+  {
+    "revision": "abb4d64c4bd8261a41e7",
+    "url": "/dRaj-Unchained/ReactNewsArticleStream/static/css/2.fc5a9450.chunk.css"
+  },
+  {
+    "revision": "f865d78f0e862996b3cd",
+    "url": "/dRaj-Unchained/ReactNewsArticleStream/static/css/main.a7f3e29b.chunk.css"
+  },
+  {
+    "revision": "abb4d64c4bd8261a41e7",
+    "url": "/dRaj-Unchained/ReactNewsArticleStream/static/js/2.f1103755.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/dRaj-Unchained/ReactNewsArticleStream/static/js/2.f1103755.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f865d78f0e862996b3cd",
+    "url": "/dRaj-Unchained/ReactNewsArticleStream/static/js/main.be7f4128.chunk.js"
+  },
+  {
+    "revision": "f79a4525ddf57bfae44c",
+    "url": "/dRaj-Unchained/ReactNewsArticleStream/static/js/runtime-main.c0ddc068.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/dRaj-Unchained/ReactNewsArticleStream/static/media/logo.ee7cd8ed.svg"
+  }
+]);
