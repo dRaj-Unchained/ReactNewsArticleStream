b93afd735d2641373e160310ae04961e9933d785
Raj Kumar Yadav <rajkumaryadav@spglobal.com>
2020-08-13 18:51:58 +0530

    Updates

---
+++ precache-manifest.a4383c9f2e09eeccfa2d93018bfd346a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fabab49874cb40cecd668500ed08c53b",
+    "url": "/ReactNewsArticleStream/index.html"
+  },
+  {
+    "revision": "abb4d64c4bd8261a41e7",
+    "url": "/ReactNewsArticleStream/static/css/2.fc5a9450.chunk.css"
+  },
+  {
+    "revision": "cdb30fb2f8e8a5af05e0",
+    "url": "/ReactNewsArticleStream/static/css/main.a7f3e29b.chunk.css"
+  },
+  {
+    "revision": "abb4d64c4bd8261a41e7",
+    "url": "/ReactNewsArticleStream/static/js/2.f1103755.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ReactNewsArticleStream/static/js/2.f1103755.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdb30fb2f8e8a5af05e0",
+    "url": "/ReactNewsArticleStream/static/js/main.d22de214.chunk.js"
+  },
+  {
+    "revision": "a6672cca974d91fe2f77",
+    "url": "/ReactNewsArticleStream/static/js/runtime-main.77e5981a.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/ReactNewsArticleStream/static/media/logo.ee7cd8ed.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dRaj-Unchained/ReactNewsArticleStream/precache-manifest.3d09f09f3663e79dd5f5d6985ba79153.js"
+  "/ReactNewsArticleStream/precache-manifest.a4383c9f2e09eeccfa2d93018bfd346a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dRaj-Unchained/ReactNewsArticleStream/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ReactNewsArticleStream/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
